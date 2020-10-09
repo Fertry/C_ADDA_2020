785dd55291a50241d9f01727cc7d3799f2ebd47d
Alejandro Fdez <alejandro@fertry.tech>
2020-10-09 20:59:26 +0200

    Creado de ficheros para la práctica 1 -  Ejercicios Iterativos

---
+++ Proyecto1AFT/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  	Análisis y Diseño de Datos y Algoritmos - 2020
+ *  	Práctica 1 - Ejercicios Iterativos
+ *      Author: Alejandro Fernández Trigo
+ */
+
+#include "main.h"
+
+
+void main() {
+
+
+
+}
+
---
+++ Proyecto1AFT/main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  	Análisis y Diseño de Datos y Algoritmos - 2020
+ *      Author: Alejandro Fernández Trigo
+ */
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// Librerias:
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "types/list.h"
+
+// Cabeceras de las funciones:
+
+
+#endif /* MAIN_H_ */
