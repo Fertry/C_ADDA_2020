b41626a8a8a18c570129b60573bf773a0dcd47bc
Alejandro Fdez <alejandro@fertry.tech>
2020-10-09 20:48:00 +0200

    Últimas funciones del tutorial de C completadas

--- Ejercicios de C/main.c
+++ Ejercicios de C/main.c
@@ -12,12 +12,15 @@ int main() {
 
 	printf("Hola mundo!\n");
 
+	char mem[256];
+	char * coordenadasX;
+
 	double numerosReales[] = { 45., 56, 90., 1., 23., 78., 76. };
 	list listaReales = list_of(numerosReales, 7, double_type);
 	long numerosEnteros[] = { 6, 7, 9, 12, 1, 7, 15, 5 };
 	list listaEnteros = list_of(numerosEnteros, 8, int_type);
-	//punto puntos[] = { {2.4, 6.7}, {3.0, 5.1}, {5.0, 1.2} };
-	//list listaPuntos = list_of(puntos, 3, punto_type);
+	punto puntos[] = { {2.4, 3.7}, {3.0, 5.1}, {5.0, 1.2} };
+	list listaPuntos = list_of(puntos, 3, punto_type);
 
 	double sumaReales = 0.0;
 	long sumaEnteros = 0;
@@ -27,12 +30,17 @@ int main() {
 	sumaReales = sumaListaReales(listaReales);
 	sumaEnteros = sumaListaEnteros(listaEnteros);
 	primerMayor = buscaPrimerMayorReal(listaReales, umbral);
-	//list listaCoordenadasX = devuelveCoordenadaXDePuntos(listaPuntos);
-	//bool verificaPredicado = cumplenPredicado(listaEnteros, es_par);
+	list listaCoordenadasX = devuelveCoordenadaXDePuntos(listaPuntos);
+	coordenadasX = list_tostring(&listaCoordenadasX, mem);
+	bool verificaPredicado = cumplenPredicado(listaEnteros, es_par);
+	punto * p = devuelvePuntoConMayorX(listaPuntos, punto_naturalorder);
 
 	printf("Suma = %0.2lf\n", sumaReales);
 	printf("Suma = %ld\n", sumaEnteros);
 	printf("El primer real mayor a %0.2lf es el %0.2lf\n", umbral, primerMayor);
+	printf("Lista coordenadas X: %s\n", coordenadasX);
+	printf("Lista puntos cumplen predidado: %s\n", verificaPredicado ? "true" : "false");
+	printf("Punto con mayor X: %s\n", punto_tostring(p, mem));
 
 }
 
@@ -126,5 +134,20 @@ bool cumplenPredicado(list lista, bool (*predicado)(int e)) {
 }
 
 // Dada una lista de puntos, buscar el de mayor coordenada X:
+punto * devuelvePuntoConMayorX(list lista, int (*cmp)(const void *, const void *)) {
 
+	punto * resultado = NULL;
+	for (int i=0;i<lista.size;i++) {
 
+		punto * p = (punto *) list_get(&lista,i);
+		if (resultado == NULL || cmp(p, resultado) > 0) {
+
+			resultado = p;
+
+		}
+
+	}
+
+	return resultado;
+
+}
--- Ejercicios de C/main.h
+++ Ejercicios de C/main.h
@@ -20,5 +20,6 @@ int sumaListaEnteros(list lista);
 double buscaPrimerMayorReal(list lista, double umbral);
 list devuelveCoordenadaXDePuntos(list lista);
 bool cumplenPredicado(list lista, bool (*predicado)(int e));
+punto * devuelvePuntoConMayorX(list lista, int (*compara)(const void *, const void *));
 
 #endif /* MAIN_H_ */
